9feb2179aa1c553f49da299a0db1b6a2622eeb22
shemil01 <shemilmk1033@gmail.com>
2024-11-04 13:50:54 +0530

    width adjested

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,16 @@
+import { ReminderSection } from "@/components/ReminderSection";
 import { HeroSection } from "@/components/user/HeroSection";
 import { Navbar } from "@/components/user/Navbar";
 
 export default function Home() {
   return (
-    <div className="h-screen w-full flex justify-center ">
-      <Navbar/>
+    <div>
+     <div className="flex justify-center"> <Navbar/></div>
       <HeroSection/>
+      <div className="flex justify-center mt-10">
+      <ReminderSection/>
+
+      </div>
     </div>
   );
 }
---
+++ src/components/ReminderSection.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+export const ReminderSection = () => {
+  return <div className=" bg-[#0E4698] w-[900px] h-[450px] rounded-3xl">
+    
+  </div>;
+};
