296ea624e3a74859f09739e4fa34886db1b6bf7f
arshad <arshadshibu45@gmail.com>
2024-11-04 14:03:31 +0530

    updated

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,12 +5,11 @@ import { Navbar } from "@/components/user/Navbar";
 export default function Home() {
   return (
     <div>
-     <div className="flex justify-center"> <Navbar/></div>
-      <HeroSection/>
-      <div className="flex justify-center mt-10">
+     <div><div className="flex justify-center"> <Navbar/></div></div>
+      <div className=" flex justify-end"><div><HeroSection/></div></div>
+      <div><div className="flex justify-center mt-10">
       <ReminderSection/>
-
-      </div>
+      </div></div>
     </div>
   );
 }
