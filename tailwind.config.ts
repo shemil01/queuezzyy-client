8710a7127f65880d6a3bfdf336dbb49c09bdd3ce
arshad <arshadshibu45@gmail.com>
2024-11-04 13:03:46 +0530

    just

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,6 +8,10 @@ const config: Config = {
   ],
   theme: {
     extend: {
+      width: {
+        'custom-1076': '1076px',
+        'custom-422': '422px',
+      },
       colors: {
         background: "var(--background)",
         foreground: "var(--foreground)",
