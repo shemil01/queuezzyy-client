3c7d09ed25e7a99ee4974b5d02eac203a3b87dd2
shemil01 <shemilmk1033@gmail.com>
2024-11-05 12:24:15 +0530

    updated

--- src/components/user/Specializations.tsx
+++ src/components/user/Specializations.tsx
@@ -20,7 +20,7 @@ export const Specializations:React.FC = () => {
         <div className=' flex justify-between p-2'><div className='font-semibold text-black'>specialization</div><div className=' font-medium text-[#0E4698]'>seeAll</div></div>
         <div className='flex flex-wrap space-x-5 p-5'>{specialization.map((items,index)=>(
             <div key={index} className='w-[163px] h-[163px] bg-slate-200 rounded-full'>
-                <Image src={items.icon} width={500} height={500}></Image>
+                <Image src={items.icon} alt='icon' width={500} height={500}></Image>
             </div>
         ))}</div>
     </div>
