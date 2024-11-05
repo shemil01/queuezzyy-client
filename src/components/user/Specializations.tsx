cf9b28293c3b08ee18a69fc1faae3b603c54223c
shemil01 <shemilmk1033@gmail.com>
2024-11-05 12:17:47 +0530

    updated

--- src/components/user/Specializations.tsx
+++ src/components/user/Specializations.tsx
@@ -7,9 +7,9 @@ icon:string
 
 }
 
-const specialization:specializations = [
-    { name: 'Cardiology', icon: '/public/orthoicon.png' },
-    { name: 'Orthopedics', icon: '/public/orthoicon.png' },
+const specialization:specializations[] = [
+    { name: 'Cardiology', icon: '/orthoicon.png' },
+    { name: 'Orthopedics', icon: '/orthoicon.png' },
     // Add more specializations as needed
   ];
 
@@ -18,9 +18,9 @@ export const Specializations:React.FC = () => {
   return (
     <div className=' font-fustat'>
         <div className=' flex justify-between p-2'><div className='font-semibold text-black'>specialization</div><div className=' font-medium text-[#0E4698]'>seeAll</div></div>
-        <div className='flex flex-wrap space-x-5 p-5'>{specialization.map((items)=>(
-            <div className='w-[163px] h-[163px] bg-slate-200 rounded-full'>
-                <Image src={items.icon} width={50} height={50}></Image>
+        <div className='flex flex-wrap space-x-5 p-5'>{specialization.map((items,index)=>(
+            <div key={index} className='w-[163px] h-[163px] bg-slate-200 rounded-full'>
+                <Image src={items.icon} width={500} height={500}></Image>
             </div>
         ))}</div>
     </div>
