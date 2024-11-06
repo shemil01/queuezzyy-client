610e6b3e63297ada6f697f307a471778242642fc
arshad <arshadshibu45@gmail.com>
2024-11-06 10:28:18 +0530

    updated

--- src/components/user/WhyUsSection.tsx
+++ src/components/user/WhyUsSection.tsx
@@ -2,9 +2,13 @@ import React from 'react'
 
 export const WhyUsSection = () => {
   return (
-    <div className=' font-fustat flex'>
-        <div className=' bg-slate-400'></div>
-        
+    <div className=' font-fustat flex justify-center p-8'>
+        <div className=' bg-slate-400 flex flex-wrap space-x-6'>
+            <div className=' flex justify-start'><div className='head'><h1 className='  text-6xl font-medium'> why us</h1></div></div>
+        <div className=' bg-slate-300 w-60 h-60 rounded-3xl flex justify-center'>
+<h1 className=' font-medium text-4xl'>instentr</h1>
+        </div>
+        </div>
     </div>
   )
 }
