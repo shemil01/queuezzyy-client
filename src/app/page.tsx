7ab876d0e643ef41959021c301c0c13af10a0885
arshad <arshadshibu45@gmail.com>
2024-11-20 11:20:51 +0530

    updated

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,25 +1,2 @@
-import { ReminderSection } from "@/components/user/ReminderSection";
-import { HeroSection } from "@/components/user/HeroSection";
-import { Navbar } from "@/components/user/Navbar";
-import { Specializations } from "@/components/user/Specializations";
-import { WhyUsSection } from "@/components/user/WhyUsSection";
-import Image from "next/image";
 
-
-export default function Home() {
-  return (
-    <div >
-     <div><div className="flex justify-center"> <Navbar/></div></div>
-      <div className=" flex justify-end mr-10 mt-10"><div><HeroSection/></div></div>
-      <div className=" mt-10"><Specializations/></div>
-      <div><div className="flex justify-start m-10">
-      
-      <ReminderSection/>
-      </div></div>
-      <div className=""><WhyUsSection/></div>
-      <div><Image src={'/svg.png'} width={1600} height={100} alt="svg"/></div>
-      
-    </div>
-  );
-}
 
