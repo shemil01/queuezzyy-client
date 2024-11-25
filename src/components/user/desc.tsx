4fca20d0711b2c3f50875a674237dff19b29d6fc
shemil01 <shemilmk1033@gmail.com>
2024-11-25 14:20:06 +0530

    updated

--- src/components/user/desc.tsx
+++ src/components/user/desc.tsx
@@ -4,7 +4,7 @@ export const Desc = () => {
   return (
     <div className="flex space-x-3">
       <div>
-        <div className="bg-[#F0DA69] w-[600px] h-[450px] rounded-3xl ">
+        <div className="bg-[#F0DA69] w-[710px] h-[450px] rounded-3xl ">
           <div className="flex-col pt-10 px-10">
             <span>.podcast</span>
 
@@ -13,10 +13,10 @@ export const Desc = () => {
         </div>
       </div>
       <div className="flex-col space-y-3">
-        <div className="bg-[#2C438A] w-[600px] h-[220px] rounded-3xl"></div>
+        <div className="bg-[#2C438A] w-[710px] h-[220px] rounded-3xl"></div>
         <div className="flex space-x-3">
-          <div className="bg-[#A3DAC2] w-[295px] h-[220px] rounded-3xl"></div>
-          <div className="bg-[#E7C2D4] w-[295px] h-[220px] rounded-3xl"></div>
+          <div className="bg-[#A3DAC2] w-[350px] h-[220px] rounded-3xl"></div>
+          <div className="bg-[#E7C2D4] w-[350px] h-[220px] rounded-3xl"></div>
         </div>
       </div>
     </div>
