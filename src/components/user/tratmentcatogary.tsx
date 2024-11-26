0cc040c171a79e474f23f255f00fc7657a2c21d7
arshad <arshadshibu45@gmail.com>
2024-11-26 15:12:54 +0530

    updated

--- src/components/user/tratmentcatogary.tsx
+++ src/components/user/tratmentcatogary.tsx
@@ -1,4 +1,4 @@
-import { Html } from "next/document";
+import Image from "next/image";
 import React from "react";
 
 interface TreatmentCatogary {
@@ -11,43 +11,66 @@ const treatmentCatogary: TreatmentCatogary[] = [
   {
     title: "Instant Video\n Consultation",
     subtitle: "Connect within 60 secs",
-    image: "/queuezzyy-client/public/treamtment catogary03.png",
+    image: "/treatment catogary01.png",
     color: "#f0da69",
   },
   {
     title: "Find Doctors\n near you",
     subtitle: "Confirmed appointments",
-    image: "/queuezzyy-client/public/treatment catogary01.png",
+    image: "/treatment catogary01.png",
     color: "#a3dac2",
   },
   {
     title: "24/7 \n Medicines",
     subtitle: "Essentials at your doorstep",
-    image: "/queuezzyy-client/public/treatment catogary02.png",
+    image: "/treatment catogary02.png",
     color: "#e7c2d4",
   },
   {
     title: "Lab\nTests",
     subtitle: "Sample pickup at your home",
-    image: "/queuezzyy-client/public/treamtment catogary03.png",
+    image: "/treamtment catogary03.png",
     color: "#92bdf6 ",
   },
 ];
 
 export const Tratmentcatogary: React.FC = () => {
   return (
-    <div className=" w-full flex flex-wrap space-x-2 font-Graphicsemibold ">
+    <div className=" w-full flex flex-wrap space-x-3 font-Graphicsemibold  relative justify-center ">
       {treatmentCatogary.map((items, index) => (
         <div
-          className="pl-[36px] pr-[36px] pt-[42px] pb-[42px] rounded-[30px] text-[#252b61]"
+          className="pl-[36px] pr-[36px] pt-[42px] pb-[42px] rounded-[30px] text-[#252b61] block -z-20 w-auto "
           style={{ backgroundColor: items.color }}
           key={index}
         >
-          <h1
-  className="font-semibold text-[28px] leading-tight"
-  dangerouslySetInnerHTML={{ __html: items.title.replace(/\n/g, "<br/>") }}
-></h1>
+          <div className=" mb-[137px] z-10">
+            <h1
+              className="font-semibold text-[28px] leading-tight mb-[18px] mt-[10px]"
+              dangerouslySetInnerHTML={{
+                __html: items.title.replace(/\n/g, "<br/>"),
+              }}
+            ></h1>
+            <p className=" font-Graphikregular mb-[10px] al">
+              {items.subtitle}
+            </p>
+          </div>
+          <div className=" pr-7 pt-4 pl-7 ">
+          <div className=
+          " w-[150px] h-[42px] bg-[#252b61] align-middle rounded-[100px] flex justify-center  mr-16 font-Graphikregular font-thin text-[20px] start-0">
+            <button className=" text-white ">Book now</button>
+          </div>
 
+          <div className="  inset-y-0  -z-10 inline-block absolute pl-1 pt-40 ">
+            <Image
+              src={items.image}
+              alt="treatment sections"
+              width={245}
+              height={245}
+              className=""
+            />
+          </div>
+          </div>
+         
         </div>
       ))}
     </div>
